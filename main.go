90dcf5f433fd5c2361cfdebbadea56f365e1a19a
Mohammad amin louragi <aminluragi.m.a.l@gmail.com>
2025-11-18 23:19:16 +0330

    linked list cycle

--- main.go
+++ main.go
@@ -9,18 +9,18 @@ import (
 func main() {
 
 	l1 := &linledList.CycleListNode{Val: 1}
-	//l2 := &linledList.CycleListNode{Val: 2}
-	// l3 := &linledList.CycleListNode{Val: 0}
-	// l4 := &linledList.CycleListNode{Val: -4}
-	// l5 := &linledList.CycleListNode{Val: 4}
-	// l6 := &linledList.CycleListNode{Val: 5}
+	l2 := &linledList.CycleListNode{Val: 2}
+	l3 := &linledList.CycleListNode{Val: 0}
+	l4 := &linledList.CycleListNode{Val: -4}
+	l5 := &linledList.CycleListNode{Val: 4}
+	l6 := &linledList.CycleListNode{Val: 5}
 
-	// l1.Next = l2
-	// l2.Next = l1
-	//  l3.Next = l4
-	// l4.Next = l2
-	// l5.Next = l6
-	// l6.Next = l3
+	l1.Next = l2
+	l2.Next = l1
+	 l3.Next = l4
+	l4.Next = l2
+	l5.Next = l6
+	l6.Next = l3
 
 	node := linledList.DetectCycle(l1)
 	if node == nil {
